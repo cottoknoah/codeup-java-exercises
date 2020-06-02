f0316fc398aab5ffdb66b577ab77279a07c7aad6
Knoah Cotto <cotto.knoah@gmail.com>
2020-06-01 20:13:28 -0500

    java practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,27 @@
 public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello, World!");
+
+            int myFavoriteNumber;
+            String myString = "myString";
+            myString = "yes";
+            long myNumber;
+
+
+            myFavoriteNumber = 204;
+            myNumber = 1007;
+
+
+
+
+            System.out.println(myFavoriteNumber++);
+            System.out.println(myString);
+            System.out.println(myNumber);
+
+
+            int x = 4;
+            x = x + 5;
+            System.out.println(++x);
+            System.out.println(x);
         }
 }
-
